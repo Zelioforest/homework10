57bc60275af374838a54ec27635c00fa5f8267e4
Zelioforest <veshky@mail.ru>
2023-11-21 03:34:08 +0300

    one

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета —  " + fullName.toUpperCase());
+        fullName = "Иванов Семён Семёнович";
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName.replace('ё', 'е'));
     }
 }
